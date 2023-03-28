9623613f629f60da918eab721e39eaca3cdbb643
Fernando Perera <fernando02perera@gmail.com>
2023-03-28 15:08:31 +0200

    Test(fixed): Implemented the intance of database and service objects into each test for not affect to the rest.

--- src/test/userSingUp.test.ts
+++ src/test/userSingUp.test.ts
@@ -1,6 +1,6 @@
 
-import {FakeDataBase} from "../core/controller/FakeUserController";
-import {UserService} from "../core/service/UserService";
+import { FakeDataBase } from "../core/controller/FakeUserController";
+import { UserService } from "../core/service/UserService";
 
 /*
  *
@@ -18,10 +18,10 @@ import {UserService} from "../core/service/UserService";
 
 describe('Sign-up', () => {
 
-    const database = new FakeDataBase()
-    const service = new UserService(database)
-
     it('dont add a email into database if the email is empty', () => {
+
+        const database = new FakeDataBase()
+        const service = new UserService(database)
 
         const email = ''
 
@@ -36,6 +36,9 @@ describe('Sign-up', () => {
 
     it('dont add a email into database if the email have a wrong format', () => {
 
+        const database = new FakeDataBase()
+        const service = new UserService(database)
+
         const email = 'example.com'
 
         const spy = jest.spyOn(service, 'save')
@@ -44,10 +47,13 @@ describe('Sign-up', () => {
 
         expect(spy).toHaveBeenCalled()
         expect(usersRecords.length).toBe(0)
-        
+
     })
 
     it('add email into database if have a correct format', () => {
+
+        const database = new FakeDataBase()
+        const service = new UserService(database)
 
         const email = 'example@gmail.com'
 
@@ -57,7 +63,24 @@ describe('Sign-up', () => {
 
         expect(spy).toHaveBeenCalled()
         expect(usersRecords.length).toBe(1)
-        
+
+    })
+
+    it('dont add email into database if the mail already exists in the database', () => {
+
+        const database = new FakeDataBase()
+        const service = new UserService(database)
+
+        const email = 'existing_mail@gmail.com'
+
+        const spy = jest.spyOn(service, 'save')
+        service.save(email)
+        service.save(email)
+        const usersRecords = service.getUserList()
+
+        expect(spy).toHaveBeenCalled()
+        expect(usersRecords.length).toBe(0)
+
     })
 
 })
