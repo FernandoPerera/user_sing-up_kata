123ef6b7a954da5a43007c8b5d839ec01e47d2cb
Fernando Perera <fernando02perera@gmail.com>
2023-03-28 14:49:56 +0200

    Test(red): dont add a email into database if the email have a wrong format test, not implemented yet.

--- src/test/userSingUp.test.ts
+++ src/test/userSingUp.test.ts
@@ -1,6 +1,5 @@
 
-
-import {FakeDataBase} from "../core/controller/FakeDataBase";
+import {FakeDataBase} from "../core/controller/FakeUserController";
 import {UserService} from "../core/service/UserService";
 
 /*
@@ -19,11 +18,25 @@ import {UserService} from "../core/service/UserService";
 
 describe('Sign-up', () => {
 
+    const database = new FakeDataBase()
+    const service = new UserService(database)
+
     it('dont add a email into database if the email is empty', () => {
 
         const email = ''
-        const database = new FakeDataBase()
-        const service = new UserService(database)
+
+        const spy = jest.spyOn(service, 'save')
+        service.save(email)
+        const usersRecords = service.getUserList()
+
+        expect(spy).toHaveBeenCalled()
+        expect(usersRecords.length).toBe(0)
+
+    })
+
+    it('dont add a email into database if the email have a wrong format', () => {
+
+        const email = 'example.com'
 
         const spy = jest.spyOn(service, 'save')
         service.save(email)
